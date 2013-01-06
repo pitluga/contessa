6ebdbde8ac44e659472418e0a99d37ffb7eb94a7
Tony Pitluga <tony.pitluga@gmail.com>
2013-01-06 17:48:24 -0600

    prepare project.clj for first release

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject contessa "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject contessa "0.1.0"
+  :description "library for GeoIP lookup via MaxMind databases"
+  :url "https://github.com/pitluga/contessa"
   :license {:name "Creative Commons Attribution-ShareAlike 3.0 Unported"
             :url "http://creativecommons.org/licenses/by-sa/3.0/legalcode"}
   :dependencies [[org.clojure/clojure "1.4.0"]]
