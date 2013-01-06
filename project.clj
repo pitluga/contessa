17b2fd25342e0ee89641e513f33e4b3be510484c
Tony Pitluga <tony.pitluga@gmail.com>
2013-01-06 16:59:40 -0600

    update license to match MaxMind

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject contessa "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "Creative Commons Attribution-ShareAlike 3.0 Unported"
+            :url "http://creativecommons.org/licenses/by-sa/3.0/legalcode"}
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :source-paths ["src/clj"]
   :java-source-paths ["src/java"])
